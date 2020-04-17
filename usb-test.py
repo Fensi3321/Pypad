35f5b963dde63b5dd43b4df42c9bc9fbf97e93cc
Bartek <barteksemeniuk57@gmail.com>
2020-04-18 01:48:50 +0200

    enough for today

--- usb-test.py
+++ usb-test.py
@@ -1,19 +1,26 @@
 from inputs import get_gamepad
 import buttons
 import gamepad
+import numpy as np 
+import cv2 as cv 
+
 
 while True:
     events = get_gamepad()
-    
-    print("RIGHT VERTICAL: ", gamepad.GamePad.get_axis("RVERTICAL"))
-    print("RIGHT HORIZONTAL", gamepad.GamePad.get_axis("RHORIZONTAL"))
-    print("LEFT HORIZONTAL",gamepad.GamePad.get_axis("LHORIZONTAL"))
-    print("LEFT VERTICAL", gamepad.GamePad.get_axis("LVERTICAL"))
 
+    input_x = gamepad.GamePad.get_axis("RHORIZONTAL")
+    input_y = gamepad.GamePad.get_axis("RVERTICAL")
+
+
+    #print("RIGHT VERTICAL: ", gamepad.GamePad.get_axis("RVERTICAL"))
+    #print("RIGHT HORIZONTAL", gamepad.GamePad.get_axis("RHORIZONTAL"))
+    #print("LEFT HORIZONTAL",gamepad.GamePad.get_axis("LHORIZONTAL"))
+    #print("LEFT VERTICAL", gamepad.GamePad.get_axis("LVERTICAL"))
 
 
     for event in events:
         if event.code == buttons.CIRCLE.get_code():
+            cv.destroyAllWindows()
             exit(0)
     
 
