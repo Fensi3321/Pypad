732d7cdd300730355636019406fc19e5c0d1a323
Bartek <barteksemeniuk57@gmail.com>
2020-04-25 03:10:20 +0200

    prep

--- usb-test.py
+++ usb-test.py
@@ -1,34 +1,13 @@
 import Pypad
-import numpy as np 
-import cv2 as cv 
 
-GamePad = Pypad.Input
-GamePad.listen()
+def test():
+    Gamepad = Pypad.Input
+    Gamepad.listen()
 
-pos_x = 320
-pos_y = 320
+    while True:
+        if Gamepad.get_key("Circle"):
+            print(Pypad.buttons.CIRCLE.get_name(), " : ", Pypad.buttons.CIRCLE.state)
+        else: print(Pypad.buttons.CIRCLE.get_name(), " : ", Pypad.buttons.CIRCLE.state)
 
-
-while True:
-    img = np.zeros((720, 1080, 3), np.uint8)
-
-    GamePad.get
-
-    x = r_stick[0]
-    y = r_stick[1]
-
-    x *= 10
-    y *= 10
-
-    x = int(x)
-    y = int(y)
-
-
-    cv.waitKey(1)
-
-    cv.rectangle(img, (pos_x, pos_y), (pos_x + 20, pos_y + 20), (255, 0, 0), 3)
-    cv.imshow("analog", img)
-
-    pos_x += x
-    pos_y += y
-
+if __name__ == "__main__":
+    test()
